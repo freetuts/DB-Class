7acd213500592e6b9268c1e8ff3977423e870daf
freetuts.org <nenad_z@ymail.com>
2015-08-11 20:29:42 +0200

    Create config.php

---
+++ example/includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+//----- your database setup -----//
+define("DB_HOST", "localhost");
+define("DATABASE", "example_db");
+define("DB_CHARSET", "utf8");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+//-------------------------------//
+
+// your other config
